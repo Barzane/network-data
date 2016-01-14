09de97e6cc49ee7f84e06e5da3051eea37b698e4
Steve Lawford <steve_lawford@yahoo.co.uk>
2016-01-14 23:36:05 +0100

    Convert full-sample .bin to .txt

--- build/code/convert_bin_to_text.py
+++ build/code/convert_bin_to_text.py
@@ -2,19 +2,24 @@
 
 import cPickle, copy
 
-def convert(year, quarter):
+def convert():
     
-    src_data = '..\\output\\data_' + str(year) + '_' + str(quarter) + '.bin'
+    src_data = '..\\output\\data_full_with_network.bin'
     
 #    data .bin loaded from \output, not moved to \temp first
     
-    print
     print 'loading', src_data
     
-    f = open(src_data, 'rb')
-    data_hold = cPickle.load(f)
-    f.close()
+    try:
+        
+        f = open(src_data, 'rb')
+        data_hold = cPickle.load(f)
+        f.close()
+
+    except IOError:
     
+        raise IOError('data unavailable', src_data)
+  
     var_list = copy.deepcopy(data_hold[data_hold.keys()[0]].keys())
     
 #    http://stackoverflow.com/questions/16581695/python-how-to-sort-lists-alphabetically-with-respect-to-capitalized-letters
@@ -34,7 +39,7 @@ def convert(year, quarter):
 
     first_key = data_dict.keys()[0].split('_')
     
-    dst_txt = '..\\output\\'+'data_' + first_key[-2] + '_' + first_key[-1] + '.txt'
+    dst_txt = '..\\output\\'+'data_full_with_network.txt'
 
     header_line = 'origin' + '\t' + 'dest' + '\t' + 'carrier' +\
         '\t' + 'year' + '\t' + 'quarter' + '\t'
@@ -69,30 +74,4 @@ def convert(year, quarter):
     f.write(output_string)
     f.close()
     
-    return None
-    
-def wrapper(test_run, test_periods, full_periods, security = None, security_max = None):
-        
-    if test_run:
-        
-        year_list = test_periods[0]
-        quarter_list = test_periods[1]
-        
-    else:
-                
-        year_list = full_periods[0]
-        quarter_list = full_periods[1]    
-    
-    for year in year_list:
-        
-        for quarter in quarter_list:
-            
-            try:
-                                
-                convert(year, quarter)
-                    
-            except IOError:
-    
-                raise IOError('data unavailable: year ' + str(year) + ' , quarter ' + str(quarter))
-
     return None
--- build/code/rundirectory.py
+++ build/code/rundirectory.py
@@ -8,14 +8,14 @@ import convert_bin_to_text, merge_bin
 
 print
 
-print 'clear contents of \output and \\temp and \input'
-
-for folder in ['..\\output\\*', '..\\temp\\*', '..\\input\\*']:
-
-    folder_contents = glob.glob(folder)
-
-    for filename in folder_contents:
-        os.remove(filename)
+#print 'clear contents of \output and \\temp and \input'
+#
+#for folder in ['..\\output\\*', '..\\temp\\*', '..\\input\\*']:
+#
+#    folder_contents = glob.glob(folder)
+#
+#    for filename in folder_contents:
+#        os.remove(filename)
 
 full_sample = False
 
@@ -27,55 +27,59 @@ else:
     
     time_periods = [(2013, 3), (2013, 4)]
 
-for (year, quarter) in time_periods:
-
-    print
-    print str(year) + 'Q' + str(quarter)
-    
-    print 'copy data_year_quarter.bin datafile from ..\data to \input'
-    
-    src = '..\\..\\data\\data_' + str(year) + '_' + str(quarter) + '.bin'
-    dst = '..\\input\\data_' + str(year) + '_' + str(quarter) + '.bin'
-    
-    shutil.copyfile(src, dst)
-    
-    print 'add network measures to data_year_quarter.bin, save to \\temp'
-    
-    add_network_measures.add_network(year, quarter)
-    
-    low_route_threshold = 10
-    
-    print 'remove carriers with low number of routes (threshold = ' + str(low_route_threshold) + '), save to \\temp'
-    
-    filter_data.filter_low_routes_by_carrier(year, quarter, low_route_threshold)
-    
-#    print 'add carrier dummies, save to \\temp'
+#for (year, quarter) in time_periods:
+#
+#    print
+#    print str(year) + 'Q' + str(quarter)
 #    
-#    carrier_dummy.add_dummies(year, quarter)
-    
-    print 'add Dai et al (2014) monopoly, duopoly, competitive dummies, save to \\temp'
-    print '...Evans & Kessides (1993) IV function called by competitive_dummy.py'
-    
-    competitive_dummy.add_dummies(year, quarter)
-    
-    print 'add airport-level marketshare and airport-level hhi, save to \\temp'
-    
-    airport_mktshr_hhi.add_variables(year, quarter)
-    
-    print 'convert bin to txt, save txt to \output'
-    
-    bin_to_txt.convert_to_txt(year, quarter)
-    
-    print 'move bin from \\temp to \output'
-    
-    src = '..\\temp\\data_' + str(year) + '_' + str(quarter) + '.bin'
-    dst = '..\\output\\data_' + str(year) + '_' + str(quarter) + '.bin'
-    
-    shutil.move(src, dst)
+#    print 'copy data_year_quarter.bin datafile from ..\data to \input'
+#    
+#    src = '..\\..\\data\\data_' + str(year) + '_' + str(quarter) + '.bin'
+#    dst = '..\\input\\data_' + str(year) + '_' + str(quarter) + '.bin'
+#    
+#    shutil.copyfile(src, dst)
+#    
+#    print 'add network measures to data_year_quarter.bin, save to \\temp'
+#    
+#    add_network_measures.add_network(year, quarter)
+#    
+#    low_route_threshold = 10
+#    
+#    print 'remove carriers with low number of routes (threshold = ' + str(low_route_threshold) + '), save to \\temp'
+#    
+#    filter_data.filter_low_routes_by_carrier(year, quarter, low_route_threshold)
+#    
+##    print 'add carrier dummies, save to \\temp'
+##    
+##    carrier_dummy.add_dummies(year, quarter)
+#    
+#    print 'add Dai et al (2014) monopoly, duopoly, competitive dummies, save to \\temp'
+#    print '...Evans & Kessides (1993) IV function called by competitive_dummy.py'
+#    
+#    competitive_dummy.add_dummies(year, quarter)
+#    
+#    print 'add airport-level marketshare and airport-level hhi, save to \\temp'
+#    
+#    airport_mktshr_hhi.add_variables(year, quarter)
+#    
+#    print 'convert bin to txt, save txt to \output'
+#    
+#    bin_to_txt.convert_to_txt(year, quarter)
+#    
+#    print 'move bin from \\temp to \output'
+#    
+#    src = '..\\temp\\data_' + str(year) + '_' + str(quarter) + '.bin'
+#    dst = '..\\output\\data_' + str(year) + '_' + str(quarter) + '.bin'
+#    
+#    shutil.move(src, dst)
 
 print 'merge .bin output files'
 
 merge_bin.wrapper(time_periods)    
+
+print 'convert .bin to .txt'
+
+convert_bin_to_text.convert()
 
 print 'move pyc files (byte code) from \code to \\temp'
 
